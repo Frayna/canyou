c223620166a71d58f41f4e4ae63db1e65fa93474
root <root@localhost.localdomain>
2016-01-08 01:14:01 +0100

    lala

--- push.sh
+++
@@ -1,9 +0,0 @@
-echo "**********************"
-echo "**     Git Push     **"
-echo "**********************\n"
-
-echo "/ Put your Commit \\"
-read -p '        ' com
-git add *
-git commit -m $com
-git push
